199f4b7870d145ab24590b09ebfe81afe6aed368
coldfish <pubg69089@gmail.com>
2021-12-04 14:37:56 +0530

    unknown user wont be able to see the post form , authentication would be required to post and see through the form

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const postController = require('../controllers/posts_controller');
-router.post('/create-post',postController.createPost);
+router.post('/create-post',passport.checkAuthentication,postController.createPost);
 module.exports = router;
