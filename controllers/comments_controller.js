b3ad50557e3b7384031a9732138bcc402816a8a7
coldfish <pubg69089@gmail.com>
2021-12-08 22:23:57 +0530

    authorized user can delete his comment

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,15 +1,15 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 
-module.exports.create = function(req, res){
-    Post.findById(req.body.post, function(err, post){
+module.exports.create = function (req, res) {
+    Post.findById(req.body.post, function (err, post) {
 
-        if (post){
+        if (post) {
             Comment.create({
                 content: req.body.content,
                 post: req.body.post,
                 user: req.user._id
-            }, function(err, comment){
+            }, function (err, comment) {
                 // handle error
 
                 post.comments.push(comment);
@@ -20,4 +20,20 @@ module.exports.create = function(req, res){
         }
 
     });
+}
+// practice
+module.exports.destroy = function (req, res) {
+    Comment.findById(req.params.id, function (err, comment) {
+        if (comment.user == req.user.id) {
+            let postId = comment.post;
+            Comment.remove();
+            Post.findByIdAndUpdate(postId, {
+                $pull: { comments: req.params.id }, function(err, post) {
+                    res.redirect('back');
+                }
+            })
+        } else {
+            return res.redirect('back');
+        }
+    })
 }
